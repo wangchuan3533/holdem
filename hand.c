4f2b1d236e00ab7d3a83939667f7c382104ea751
wangchuan3533 <wangchuan3533@gmail.com>
2015-12-12 10:01:44 +0800

    fix straight check
    A, 2, 3, 4, 5 is also straight, and should be smallest

--- hand.c
+++ hand.c
@@ -85,6 +85,9 @@ hand_rank_t _calc_rank(hand_t hand)
         }
         if (j == 5) {
             straight = 1;
+        } else if (i == 4 && j == 5 && numbers[CARD_BASE - 1] > 0) {
+            straight = 1;
+            numbers[CARD_BASE - 1] = 0;
         }
     }
 
